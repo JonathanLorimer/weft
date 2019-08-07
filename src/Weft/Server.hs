7f2f4f6ca5b2a183a4927a5686a77737f3be961e
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-08-07 17:26:45 -0400

    variables working

--- src/Weft/Server.hs
+++ src/Weft/Server.hs
@@ -23,7 +23,8 @@ import Data.Maybe
 import qualified Data.ByteString.Lazy as BL
 import Control.Monad.Reader
 import Data.Text (Text)
-import Data.Text.Encoding
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as HM
 import qualified Data.Map as M
 
 
@@ -38,35 +39,50 @@ data ClientRequest =
   ClientRequest { queryContent  :: Text
                 , variables     :: Vars
                 , operationName :: Maybe Text
-                }
+                } deriving (Show, Eq, Ord)
 
 maybeClientRequest :: C8.ByteString -> Maybe ClientRequest
 maybeClientRequest rb = do
   json  <- decode @Value $ BL.fromStrict rb
   q     <- json ^? key "query" . _String
   let v = fromMaybe M.empty $ do
-      vars     <- json ^? key "variables" . _String
-      listVars <- decode . BL.fromStrict . encodeUtf8 $ vars
-      pure     $  M.fromList listVars
+            vars     <- json ^? key "variables" . _Object
+            pure     $  M.fromList . stringify . HM.toList $ vars
   let o = json ^? key "operationName" . _String
   pure $ ClientRequest q v o
 
+stringify :: [(Text, Value)] -> [(String, String)]
+stringify = fmap f
+  where 
+    f (t,v) = ( T.unpack t
+              , C8.unpack . BL.toStrict . encode $ v 
+              )
 
 
 note :: Maybe a -> Either String a
 note Nothing = Left "maybeClientRequest failed"
 note (Just x) = Right x
 
-app :: (ToJSON (q 'Response), Wefty q) => Gql q m s 'Resolver -> Application
+app :: (ToJSON (q 'Response), Wefty q, Show (q 'Query)) => Gql q m s 'Resolver -> Application
 app resolver req f = do
 #if MIN_VERSION_wai(3,2,2)
   rb <- getRequestBodyChunk req
 #else
   rb <- requestBody req
 #endif
+  putStrLn "request body"
+  print rb
+  putStrLn "---"
+  let m = note . maybeClientRequest $ rb
+  putStrLn "maybeClientRequest"
+  print m
+  putStrLn "---"
   let _eitherQuery = do
         clientRequest <- note . maybeClientRequest $ rb
         parseReqBody clientRequest
+  putStrLn "either query"
+  print _eitherQuery
+  putStrLn "---"
   case _eitherQuery of
           Right query' -> do
             res <- resolve resolver query'
@@ -79,7 +95,7 @@ successResponse = responseLBS status200 [(hContentType, "application/json")] . e
 errorResponse :: BL.ByteString -> Response
 errorResponse = responseLBS status500 [(hContentType, "application/json")]
 
-server :: (Wefty q)
+server :: (Wefty q, Show (q 'Query))
        => [Settings -> Settings]
        -> Gql q m s 'Resolver
        -> IO ()
