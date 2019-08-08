a1e8aed3fb20a84da1a69fa30189ca03bc391e50
Sandy Maguire <sandy@sandymaguire.me>
2019-08-08 14:05:54 -0400

    json response

--- src/Weft/Generics/JSONResponse.hs
+++ src/Weft/Generics/JSONResponse.hs
@@ -33,16 +33,43 @@ combine :: Value -> Value -> Value
 combine (Object a) (Object b) = Object $ a <> b
 combine _ _                   = error "combine failed in JSONResponse, this should not have happened"
 
-instance {-# OVERLAPPING #-} (HasJSONResponse record)
-      => GJsonResponse (K1 x (M.Map T.Text [record 'Response])) where
-    gJsonResponse (K1 r) = toJSON $ fmap jsonResponse <$> r
+class GJsonTerm (t :: *) where
+  gJsonTerm :: t -> Value
 
-instance {-# OVERLAPPING #-} (HasJSONResponse record)
-      => GJsonResponse (K1 x (M.Map T.Text (record 'Response))) where
-    gJsonResponse (K1 r) = toJSON $ jsonResponse <$> r
+instance GJsonTerm t => GJsonResponse (K1 x t) where
+  gJsonResponse (K1 t) = gJsonTerm t
 
-instance {-# OVERLAPPING #-} (ToJSON record)
-      => GJsonResponse (K1 x (M.Map T.Text record)) where
-    gJsonResponse (K1 r) = toJSON r
+instance GJsonTerm (Magic ts t) => GJsonTerm (ToMagic ts t) where
+  gJsonTerm = gJsonTerm . unMagic
+
+instance GJsonResponse (M1 _1 _2 _3) => GJsonTerm (M1 _1 _2 _3 _4) where
+  gJsonTerm = gJsonResponse
+
+instance GJsonTerm t => GJsonTerm (M.Map T.Text t) where
+  gJsonTerm = toJSON . fmap gJsonTerm
+
+instance GJsonTerm t => GJsonTerm [t] where
+  gJsonTerm = toJSON . fmap gJsonTerm
+
+instance HasJSONResponse record => GJsonTerm (record 'Response) where
+  gJsonTerm = jsonResponse
+
+instance GJsonTerm Int where
+  gJsonTerm = toJSON
+
+instance GJsonTerm Integer where
+  gJsonTerm = toJSON
+
+instance GJsonTerm Float where
+  gJsonTerm = toJSON
+
+instance GJsonTerm Double where
+  gJsonTerm = toJSON
+
+instance GJsonTerm Bool where
+  gJsonTerm = toJSON
+
+instance {-# OVERLAPPING #-} GJsonTerm String where
+  gJsonTerm = toJSON
 
 
