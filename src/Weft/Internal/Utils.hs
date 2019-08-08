ed28c5948424325172b577297fe4cff9055085c5
Sandy Maguire <sandy@sandymaguire.me>
2019-08-08 14:40:44 -0400

    more utils

--- src/Weft/Internal/Utils.hs
+++ src/Weft/Internal/Utils.hs
@@ -7,3 +7,9 @@ import Data.Generic.HKD.Build
 buildQuery :: forall rec k. Build rec (ToMagic 'Query) k => k
 buildQuery = build @rec @(ToMagic 'Query)
 
+buildResponse :: forall rec k. Build rec (ToMagic 'Response) k => k
+buildResponse = build @rec @(ToMagic 'Response)
+
+buildResolver :: forall rec k. Build rec (ToMagic 'Resolver) k => k
+buildResolver = build @rec @(ToMagic 'Resolver)
+
